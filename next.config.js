47dc08c5fcf9c4441698379fc264af0efa903d31
ASUSX360U\Трещина <oleg.lekhnitsky@gmail.com>
2023-02-05 14:00:15 +0300

    hello

---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  reactStrictMode: false,
+  images: {
+    domains: ['www.notion.so', "lh3.googleusercontent.com"]
+  },
+}
---
+++ src/components/Layout/Header/index.tsx
@@ -0,0 +1,31 @@
+import { useRef } from "react"
+import CONFIG from "site.config"
+import NavBar from "./NavBar"
+import Logo from "./Logo"
+import ThemeToggle from "./ThemeToggle"
+
+type Props = {
+  fullWidth: boolean
+}
+
+const Header: React.FC<Props> = ({ fullWidth }) => {
+  const navRef = useRef(null)
+
+  return (
+    <div
+      className={`sticky-nav m-0 w-full h-3 flex flex-row justify-between items-center mb-2 md:mb-4 py-8 bg-opacity-100 max-w-6x2 px-4 ${
+        fullWidth && "px-2 md:px-24"
+      }`}
+      id="sticky-nav"
+      ref={navRef}
+    >
+      <Logo />
+      <div className={`flex gap-3 items-center`}>
+        <ThemeToggle />
+        <NavBar />
+      </div>
+    </div>
+  )
+}
+
+export default Header
