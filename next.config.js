f57ee09b1001a4c1767a4ecd07deb9f4e6132090
oleg-lekhnitsky <oleg.lekhnitsky@gmail.com>
2023-02-09 01:13:15 +0300

    access

--- next.config.js
+++ next.config.js
@@ -4,3 +4,14 @@ module.exports = {
     domains: ['www.notion.so', "lh3.googleusercontent.com", "yuliananaida.pages.dev", "s3-us-west-2.amazonaws.com", "lh5.googleusercontent.com"]
   },
 }
+
+// In your server.js or next.config.js file
+const httpHeaders = require('http-headers');
+
+module.exports = (req, res) => {
+  httpHeaders(res, {
+    'Access-Control-Allow-Origin': '*',
+  });
+
+  // The rest of your code here...
+};
